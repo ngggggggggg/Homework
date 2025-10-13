77c13e37e32681c9897f2ecded498be78f2de31c
ngggggggggg <41343150@nfu.edu.tw>
2025-10-13 13:48:19 +0800

    Update header.h

--- homework1/src/header.h
+++ homework1/src/header.h
@@ -1,1 +1,10 @@
-nigger
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <string>
